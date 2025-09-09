6e5c884e080b02843493db8f2e2ef1ae9933b0cb
Manning Unger <manningunger@gmail.com>
2025-09-09 01:13:15 -0400

    git icons, show diagnostics next to directories that contain bad files

--- lua/native/opts/netrw.lua
+++ lua/native/opts/netrw.lua
@@ -48,8 +48,8 @@ local NETRW_INDENT_WIDTH = 2
 -- Returns a mapping of the line number in the netrw tree
 -- to the full filename at that line number
 ---@param bufnr integer buffer number of the neovim window
----@return { [integer]: string } 
-local function netrw_tree_line_number_to_filename(bufnr)
+---@return { [integer]: table } 
+local function netrw_tree_line_number_to_file_info(bufnr)
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
   local running_dirpath = netrw_tree_get_root(bufnr) or vim.fs.normalize(vim.fn.getcwd()) .. "/"
   local prev_indent_level = 0
@@ -66,6 +66,9 @@ local function netrw_tree_line_number_to_filename(bufnr)
     if curr_indent_level ~= 0 then
       local indent_difference = prev_indent_level - curr_indent_level
       while (indent_difference > 0 or prev_was_dir and indent_difference >= 0) do
+        if prev_was_dir then
+          filenames[i - 2].expanded = false
+        end
         indent_difference = indent_difference - NETRW_INDENT_WIDTH
         running_dirpath = parent_dir(running_dirpath)
       end
@@ -77,7 +80,11 @@ local function netrw_tree_line_number_to_filename(bufnr)
       if not filename then
         error("unreachable: netrw line " .. filename .. " without filename")
       end
-      filenames[i - 1] = running_dirpath .. filename
+      filenames[i - 1] = {
+        filename = running_dirpath .. filename,
+        directory = not not maybe_curr_dirname,
+        expanded = maybe_curr_dirname and true or nil
+      }
       prev_was_dir = maybe_curr_dirname and true or false
       prev_indent_level = curr_indent_level
     end
@@ -96,7 +103,7 @@ local function get_netrw_tree_buffers()
 end
 
 
-local netrw_extmark_namespace = vim.api.nvim_create_namespace("netrw_extmark_namespace")
+local netrw_extmark_diagnostic_namespace = vim.api.nvim_create_namespace("netrw_extmark_diagnostic_namespace")
 
 local severity_to_hl_group = {
   [vim.diagnostic.severity.HINT] = vim.api.nvim_get_hl_id_by_name("DiagnosticHint"),
@@ -105,13 +112,44 @@ local severity_to_hl_group = {
   [vim.diagnostic.severity.ERROR] = vim.api.nvim_get_hl_id_by_name("DiagnosticError"),
 }
 
-local function draw_diagnostic(bufnr, line, col, severity)
-  vim.api.nvim_buf_set_extmark(bufnr, netrw_extmark_namespace, line, col, {
+-- local severity_to_underline_hl_group = {
+--   [vim.diagnostic.severity.HINT] = vim.api.nvim_get_hl_id_by_name("DiagnosticUnderlineHint"),
+--   [vim.diagnostic.severity.INFO] = vim.api.nvim_get_hl_id_by_name("DiagnosticUnderlineInfo"),
+--   [vim.diagnostic.severity.WARN] = vim.api.nvim_get_hl_id_by_name("DiagnosticUnderlineWarn"),
+--   [vim.diagnostic.severity.ERROR] = vim.api.nvim_get_hl_id_by_name("DiagnosticUnderlineError"),
+-- }
+
+
+local function append_extmark(bufnr, line, ns, text, hl_group)
+  vim.api.nvim_buf_set_extmark(bufnr, ns, line, 0, {
     virt_text={
-      {"  " .. vim.diagnostic.config().signs.text[severity], severity_to_hl_group[severity]}
+      {text, hl_group}
     },
-    virt_text_pos = "overlay",
+    priority=2
   })
+end
+
+local function draw_diagnostic(bufnr, line, severity)
+  append_extmark(bufnr, line, netrw_extmark_diagnostic_namespace, vim.diagnostic.config().signs.text[severity], severity_to_hl_group[severity])
+end
+
+vim.api.nvim_set_hl(0, "GitUnstaged", { fg = "#FFFF00" })
+vim.api.nvim_set_hl(0, "GitStaged", { fg = "#00FF00" })
+vim.api.nvim_set_hl(0, "GitUnmerged", { fg = "#FF0000" })
+
+local netrw_extmark_git_namespace = vim.api.nvim_create_namespace("netrw_extmark_git_namespace")
+
+local function draw_git_icons(bufnr, line, is_unstaged, is_staged, is_unmerged)
+  if is_unstaged then
+    local hl_id = vim.api.nvim_get_hl_id_by_name("GitUnstaged")
+    append_extmark(bufnr, line, netrw_extmark_git_namespace, "M", hl_id)
+  end
+  if is_staged then
+    append_extmark(bufnr, line, netrw_extmark_git_namespace, "A", vim.api.nvim_get_hl_id_by_name("GitStaged"))
+  end
+  if is_unmerged then
+    append_extmark(bufnr, line, netrw_extmark_git_namespace, "󰘭", vim.api.nvim_get_hl_id_by_name("GitUnmerged"))
+  end
 end
 
 ---@param bufnr integer? buffer to populate diagnostics on. nil for all netrw bufs.
@@ -123,24 +161,95 @@ local function populate_netrw_diagnostics(bufnr)
     return
   end
 
-  vim.api.nvim_buf_clear_namespace(bufnr, netrw_extmark_namespace, 0, -1)
+  vim.api.nvim_buf_clear_namespace(bufnr, netrw_extmark_diagnostic_namespace, 0, -1)
   local diagnostics = max_diag_severity_by_file()
-  local line_number_to_filename = netrw_tree_line_number_to_filename(bufnr)
+  local line_number_to_file_info = netrw_tree_line_number_to_file_info(bufnr)
 
-  for line_num, filename in pairs(line_number_to_filename) do
+  for line_num, file_info in pairs(line_number_to_file_info) do
+    local filename = file_info.filename
     if diagnostics[filename] then
-      draw_diagnostic(bufnr, line_num, -1, diagnostics[filename])
+      draw_diagnostic(bufnr, line_num, diagnostics[filename])
+    elseif file_info.expanded == false then
+      -- Show nested messages in expanded dirs
+      local min_severity = vim.diagnostic.severity.HINT + 1
+      for candidate_filename, severity in pairs(diagnostics) do
+        if string.find(candidate_filename, "^" .. file_info.filename) and severity < min_severity then
+          min_severity = severity
+        end
+      end
+      if min_severity ~= vim.diagnostic.severity.HINT + 1 then
+        draw_diagnostic(bufnr, line_num, min_severity)
+      end
     end
   end
 end
-populate_netrw_diagnostics()
+
+local function split(s, delimiter)
+  local result = {}
+  for match in string.gmatch(s, "([^"..delimiter.."]+)") do
+      table.insert(result, match)
+  end
+  return result
+end
+
+local function to_absolute_paths(relative_paths)
+  local result = {}
+  for _, path in ipairs(relative_paths) do
+    table.insert(result, vim.fs.normalize(vim.fn.fnamemodify(path,":p")))
+  end
+  return result
+end
+
+local function git_unstaged_changes()
+  return to_absolute_paths(split(vim.fn.system("git diff --name-only"), "\n"))
+end
+
+local function git_staged_changes()
+  return to_absolute_paths(split(vim.fn.system("git diff --name-only --staged"), "\n"))
+end
+
+local function git_unmerged_changes()
+  return to_absolute_paths(split(vim.fn.system("git diff --name-only --diff-filter=U"), "\n"))
+end
+
+local function list_to_set(list)
+  local set = {}
+  for _, val in ipairs(list) do
+    set[val] = true
+  end
+  return set
+end
+
+
+local function populate_netrw_git_icons(bufnr)
+  if not bufnr then
+    for _, netrw_bufnr in ipairs(get_netrw_tree_buffers()) do
+      populate_netrw_git_icons(netrw_bufnr)
+    end
+    return
+  end
+
+  vim.api.nvim_buf_clear_namespace(bufnr, netrw_extmark_git_namespace, 0, -1)
+  local unstaged = list_to_set(git_unstaged_changes())
+  local staged = list_to_set(git_staged_changes())
+  local unmerged = list_to_set(git_unmerged_changes())
+
+  local line_number_to_file_info = netrw_tree_line_number_to_file_info(bufnr)
+
+  for line_num, file_info in pairs(line_number_to_file_info) do
+    local filename = file_info.filename
+    draw_git_icons(bufnr, line_num, unstaged[filename], staged[filename], unmerged[filename])
+  end
+end
 
 vim.diagnostic.handlers["netrw/redraw_diagnostics"] = {
   show = function(_, _, _, _)
     populate_netrw_diagnostics()
+    populate_netrw_git_icons()
   end,
   hide = function(_, _)
     populate_netrw_diagnostics()
+    populate_netrw_git_icons()
   end
 }
 
@@ -148,20 +257,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern="netrw",
   callback = function()
     populate_netrw_diagnostics()
+    populate_netrw_git_icons()
   end
 })
 
--- local last_netrw_lines = {}
-
--- vim.api.nvim_create_autocmd({"CursorHold", "CursorMoved"}, {
---   pattern = "*NetrwTreeListing*",
---   callback = function()
---     local buf = vim.api.nvim_get_current_buf()
---     local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
---     local hash = table.concat(lines, "\n")
---     if hash ~= last_netrw_lines[buf] then
---       print("buf", buf)
---       populate_netrw_diagnostics(buf)
---     end
---   end
--- })

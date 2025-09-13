bc052dcd08a4af55708299c587dd1ffc1950ad4f
Manning Unger <manningunger@gmail.com>
2025-09-12 20:12:34 -0400

    add LSP binding for goto definition

--- lua/native/lsp/keymaps.lua
+++ lua/native/lsp/keymaps.lua
@@ -1,1 +1,11 @@
 -- LSP keymaps
+local function goto_definition()
+  local clients = vim.lsp.get_clients()
+  if clients and #clients > 0 then
+    vim.lsp.buf.definition()
+  else
+    vim.cmd("normal gd")
+  end
+end
+
+vim.keymap.set("n", "gd", goto_definition, { noremap = true, desc = "LSP goto definition" })

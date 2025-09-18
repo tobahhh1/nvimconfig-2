69229c8cacbd8be87d0f7e3cb921a7440bb13c66
Manning Unger <manningunger@gmail.com>
2025-09-17 20:26:38 -0400

    change vthird to use third of width of entire screen

--- lua/native/user_command/window.lua
+++ lua/native/user_command/window.lua
@@ -1,7 +1,7 @@
 vim.o.splitright = true
 
 vim.api.nvim_create_user_command("Vthird", function()
-  local desired_width = math.floor(vim.fn.winwidth(0) / 3)
+  local desired_width = math.floor(vim.o.columns / 3)
   vim.cmd("vsplit")
   vim.api.nvim_win_set_width(0, desired_width)
 end, {})

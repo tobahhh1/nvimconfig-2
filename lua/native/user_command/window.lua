62dfe9d1bb8e3727351fa966a8046bb800e80b39
Manning Unger <manningunger@gmail.com>
2025-12-28 11:55:08 -0500

    vertical split third

--- lua/native/user_command/window.lua
+++ lua/native/user_command/window.lua
@@ -1,4 +1,5 @@
 vim.o.splitright = true
+vim.o.splitbelow = true
 
 local function vthird(obj)
   local desired_width = math.floor(vim.o.columns / 3)
@@ -12,3 +13,13 @@ end
 vim.api.nvim_create_user_command("Vthird", vthird, { nargs = "*" })
 vim.keymap.set({"n", "v"}, "<C-w>V", "<cmd>Vthird<cr>", {silent = true})
 
+local function hthird(obj)
+  local desired_height = math.floor(vim.o.lines / 3)
+  vim.cmd("split")
+  vim.api.nvim_win_set_height(0, desired_height)
+  if obj.args ~= "" then
+    vim.cmd(obj.args)
+  end
+end
+vim.api.nvim_create_user_command("Hthird", hthird, { nargs = "*" })
+vim.keymap.set({"n", "v"}, "<C-w>S", "<cmd>Hthird<cr>", {silent = true})

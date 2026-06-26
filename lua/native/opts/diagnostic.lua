59a9e5c804f41aed175bd6b39b5dafcebee12a06
Manning Unger <manningunger@gmail.com>
2026-06-26 09:16:58 -0400

    Add diagnostic keymap

--- lua/native/opts/diagnostic.lua
+++ lua/native/opts/diagnostic.lua
@@ -13,3 +13,23 @@ vim.diagnostic.config({
   ["netrw/redraw_diagnostics"] = {}
 })
 
+vim.api.nvim_create_user_command("DiagnosticsToQuickfix", function(opts)
+  if #opts.fargs == 0 then
+    vim.diagnostic.setqflist({ open = true })
+  else
+    -- pass severity of each argument to a table
+    local diagnostic_severities = {}
+    for _, arg in ipairs(opts.fargs) do
+      local severity = vim.diagnostic.severity[string.upper(arg)]
+      if severity then
+        table.insert(diagnostic_severities, severity)
+      else
+        print("Invalid severity: " .. arg)
+        return
+      end
+    end
+    vim.diagnostic.setqflist({ open = true, severity = diagnostic_severities })
+  end
+end, { nargs = "*" })
+
+vim.api.nvim_set_keymap('n', '<leader>qd', ':DiagnosticsToQuickfix<CR>', { noremap = true, silent = true })
